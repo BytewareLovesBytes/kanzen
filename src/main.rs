1e9aaed6598a57f6e9c222890c6e78b012d380db
justanotherbyte <wishymovies@gmail.com>
2023-04-27 18:40:50 +0100

    remove poise command shown in documentation

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,6 @@ pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, Data, Error>;
 pub type Command = poise::Command<Data, Error>;
 
-/// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
-async fn age(
-    ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
-) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() {
     let conf: Config = toml::from_str(
